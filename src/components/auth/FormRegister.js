f800f2564c64ddf6d8c477d291c0a8576806bdf2
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2021-02-13 14:07:47 -0500

    Obtener nick, email, password del FormRegister

--- src/components/auth/FormRegister.js
+++ src/components/auth/FormRegister.js
@@ -1,12 +1,50 @@
 import React from 'react';
+import useForm from '../../hooks/useForm';
 
 const FormRegister = () => {
+
+    const [formValue, handleInputChange ] = useForm({
+      nick:'',
+      email:'',
+      password:'',
+      passwordVerificar:''
+    });
+
+
+    const { nick, email, password, passwordVerificar } = formValue;
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log( nick, email, password, passwordVerificar );
+    }
+
+
     return (
-        <form>
-        <input type="text" name="nick" id="nick" />            
-        <input type="text" name="mail" id="mail" />
-        <input type="password" name="password" id="password" />
-        <input type="password" name="password-verificar" id="password-verificar" />  
+        <form onSubmit={ handleSubmit }>
+        <input type="text" 
+          name="nick" 
+          id="nick"
+          value = { nick } 
+          onChange={ handleInputChange }
+        />            
+        <input type="text" 
+          name="email" 
+          id="email" 
+          value={ email }
+          onChange = { handleInputChange }
+        />
+        <input type="password" 
+          name="password" 
+          id="password" 
+          value={ password }
+          onChange = { handleInputChange }
+        />
+        <input type="password" 
+          name="passwordVerificar" 
+          id="passwordVerificar" 
+          value = { passwordVerificar }
+          onChange ={ handleInputChange }
+        />  
         <button className="btn btn-primary btn-block">Crear una cuenta</button>
         <p className="text-center">O regístrate usando</p>
       </form>
